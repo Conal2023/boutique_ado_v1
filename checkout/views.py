9230f63397d16e246e2867cb84badc0586a2cf70
Conal McCrory <conalmccrory@gmail.com>
2024-04-28 17:13:02 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51PAYrA00UVDEpv8elMI25s9CDvxrMvVBI00n1O4ShdSYd0EFED8sibAjUQUXVqu86vymd6OhaJL0GXxYBaZL46wP00Q58gneJz',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
