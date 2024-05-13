257bfc4b3d014a11c312430ca26d12ca9d02d1f5
Conal McCrory <conalmccrory@gmail.com>
2024-05-13 08:35:49 +0000

    056 deployment part 2

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['boutique-ado-conal.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['boutique-ado-conal-c8f10505c2fb.herokuapp.com', 'localhost']
 
 
 # Application definition
