86498165cd79659d36d704585697ef4e140349d2
Conal McCrory <conalmccrory@gmail.com>
2024-05-13 17:50:17 +0000

    059 - deployment part 5

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -177,7 +177,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
     # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'boutique-ado-conal1'
+    AWS_STORAGE_BUCKET_NAME = 'boutique-ado-conal'
     AWS_S3_REGION_NAME = 'eu-north-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
